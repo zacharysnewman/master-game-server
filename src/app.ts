e87e85a97c95e6916562b5927029b1d971eb2934
Zack Newman <zacknewman@comcast.net>
2020-10-11 15:45:54 -0700

    Setup to remove old players and show all connected players.

--- src/app.ts
+++ src/app.ts
@@ -6,7 +6,7 @@ type Callback = (req, res) => unknown;
 const appHandle = (handle: string) => (callback: Callback) =>
   app.all(handle, callback);
 
-const handleHeartbeat = () => appHandle("/heartbeat")(heartbeat);
+const handleHeartbeats = () => appHandle("/heartbeat")(heartbeat);
 
 const listenForConnections = (port: string | number) =>
   app.listen(port, () => console.log(`App listening at ${port}`));
@@ -14,48 +14,48 @@ const listenForConnections = (port: string | number) =>
 //
 type Player = {
   name: string;
-  remote:{ ip: string,
-  port: number | string };
-  local: { ip:string, port: number|string};
+  ip: string; 
+  port: number | string;
   lastHeartbeat: number;
 };
 
 const PLAYER_TIMEOUT = 10000; // ms
-let players: Player[] = [];
+let allPlayers: Player[] = [];
 
 const heartbeat: Callback = (req, res) => {
-  const name = req.query.playerName;
-  const remoteIp = req.connection.remoteAddress;
-  const remotePort = req.connection.remotePort;
-  const localIp = req.connection.localAddress;
-  const localPort = req.connection.localPort;
   const now = Date.now();
 
-  const thisPlayer: Player = { name, remote: { ip: remoteIp, port: remotePort }, local: { ip: localIp, port: localPort }, lastHeartbeat: now };
+  const thisPlayer: Player = { 
+    name: req.query.playerName, 
+    ip: req.connection.remoteAddress, 
+    port: req.connection.remotePort, 
+    lastHeartbeat: now 
+  };
 
-  // Maybe update this search to only check for ip/port
-  if (players.includes(thisPlayer)) {
-    // Update player's heartbeat, etc. (and maybe name)
+  const playerIndex = findPlayerIndex(allPlayers)(thisPlayer);
+
+  if (playerIndex) {
+    allPlayers[playerIndex] = thisPlayer;
   } else {
-    players.push(thisPlayer);
+    allPlayers.push(thisPlayer);
   }
 
-  players = players.filter(playerFilter(now));
-
-  const playersWithoutThisPlayer = players;//players.filter(
-  //   (player) => player !== thisPlayer
-  // );
-
-  res.send(JSON.stringify(playersWithoutThisPlayer));
+  allPlayers = allPlayers.filter(playersWithRecentHeartbeats(now));
+  
+  res.send(JSON.stringify(allPlayers));
 };
 
-const playerFilter = (now: number) => (player: Player) =>
+const includesPlayer = (players: Player[]) => (player: Player) => !!players.find((x) => x.ip === player.ip && x.port === player.port);
+const findPlayerIndex = (players: Player[]) => (player: Player) => players.findIndex((x) => x.ip === player.ip && x.port === player.port);
+const allPlayersExceptThisOne = (player: Player) => allPlayers.filter((x) => x !== player);
+
+const playersWithRecentHeartbeats = (now: number) => (player: Player) =>
   now - player.lastHeartbeat < PLAYER_TIMEOUT;
 
 const main = () => {
   const PORT: string | number = process.env.PORT || 3000;
 
-  handleHeartbeat();
+  handleHeartbeats();
   listenForConnections(PORT);
 };
 
--- tests/httptests.ts
+++ tests/httptests.ts
@@ -2,6 +2,15 @@ const request = require("postman-request");
 
 const payload = "Zack";
 
+// request(
+//   `http://localhost:3000/heartbeat?playerName=${payload}`,
+//   (error, response, body) => {
+//     console.log("error:", error); // Print the error if one occurred
+//     console.log("statusCode:", response && response.statusCode); // Print the response status code if a response was received
+//     console.log("body:", body); // Print the HTML for the Google homepage.
+//   }
+// );
+
 request(
   `http://localhost:3000/heartbeat?playerName=${payload}`,
   (error, response, body) => {
