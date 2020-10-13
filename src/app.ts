76dfce9707c615524c5b10b85c27a78b0074d897
Zack Newman <zacknewman@comcast.net>
2020-10-13 10:55:32 -0700

    Setup unified player identification logic.

--- src/app.ts
+++ src/app.ts
@@ -20,6 +20,22 @@ type Player = {
 };
 
 const PLAYER_TIMEOUT = 10000; // ms
+
+const replaceAll = (searchTerm: string) => (replacement: string) => (value: string) => value.split(searchTerm).join(replacement);
+const removeColons = (value: string) => replaceAll(":")("")(value);
+const removeFs = (value: string) => replaceAll("f")("")(value);
+
+const isPlayer = (player: Player) => (x: Player) => x.ip === player.ip && x.name === player.name;
+const isNotPlayer = (player: Player) => (x: Player) => !isPlayer(player)(x);
+const includesPlayer = (players: Player[]) => (player: Player) => !!players.find(isPlayer(player));
+const findPlayerIndex = (players: Player[]) => (player: Player) => players.findIndex(isPlayer(player));
+const allPlayersExceptThisPlayer = (players: Player[]) => (player: Player) => players.filter(isNotPlayer(player));
+
+const updatePlayer = (newPlayerData: Player) => (playerToUpdate: Player) => ({...playerToUpdate, lastHeartbeat: newPlayerData.lastHeartbeat});
+
+const playersWithRecentHeartbeats = (now: number) => (player: Player) => 
+  now - player.lastHeartbeat < PLAYER_TIMEOUT;
+
 let allPlayers: Player[] = [];
 
 const heartbeat: Callback = (req, res) => {
@@ -41,35 +57,16 @@ const heartbeat: Callback = (req, res) => {
 
   const playerIndex = findPlayerIndex(allPlayers)(thisPlayer);
 
-  // console.log("Players before push:", allPlayers);
-
   if (playerIndex >= 0) {
-    // console.log("modified");
-    allPlayers[playerIndex] = thisPlayer;
+    allPlayers[playerIndex] = updatePlayer(thisPlayer)(allPlayers[playerIndex]);
   } else {
-    // console.log("pushed");
     allPlayers.push(thisPlayer);
   }
 
-  // console.log("Players after push:", allPlayers);
-
   allPlayers = allPlayers.filter(playersWithRecentHeartbeats(now));
-
-  // console.log("Players after heartbeat filter:", allPlayers);
   
   res.send(JSON.stringify(allPlayers));
 };
-
-const replaceAll = (searchTerm: string) => (replacement: string) => (value: string) => value.split(searchTerm).join(replacement);
-const removeColons = (value: string) => replaceAll(":")("")(value);
-const removeFs = (value: string) => replaceAll("f")("")(value);
-
-const includesPlayer = (players: Player[]) => (player: Player) => !!players.find((x) => x.ip === player.ip && x.port === player.port);
-const findPlayerIndex = (players: Player[]) => (player: Player) => players.findIndex((x) => x.ip === player.ip && x.port === player.port);
-const allPlayersExceptThisOne = (player: Player) => allPlayers.filter((x) => x !== player);
-
-const playersWithRecentHeartbeats = (now: number) => (player: Player) =>
-  now - player.lastHeartbeat < PLAYER_TIMEOUT;
 
 const main = () => {
   const PORT: string | number = process.env.PORT || 3000;
